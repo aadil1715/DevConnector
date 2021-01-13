c808d2a9eee801d82eb2594ad4149d64b2ded5cd
aadil1715 <aadilshaikh862@gmail.com>
2021-01-13 23:37:37 +0530

    Login functionality done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/Users');
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 //@route GET api/auth
 //@desc Test route
@@ -15,5 +19,66 @@ router.get('/',auth,async(req,res) => {
         return res.status(500).send('Server Error');
     }
 });
+
+router.post(
+    "/",
+    [
+      check("email", "Valid email please").isEmail(),
+      check("password", "Please enter a password").exists(),
+    ],
+    async (req, res) => {
+      //This is called destructuring.
+      const {email, password } = req.body; 
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      try {
+        //Check if the user exists.
+        let user = await User.findOne({ email });
+        //console.log(password+' ' + user);
+        if (!user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "Invalid Credentials" }] });
+        }
+
+        
+        
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch)
+        {
+            return res
+            .status(400)
+            .json({ errors: [{ msg: "Invalid Credentials" }] });   
+        }
+      
+        const payload = {
+          user:{
+            id: user.id 
+          }
+        }
+  
+        jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          {expiresIn:36000},
+          (err,token) => {
+            if(err) throw err
+            res.json({token});
+          }
+          );
+  
+          //TOKEN CREATED NOW HANDLE THE VERIFICATION
+  
+        //res.send("User registered");
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+      }
+    }
+  );
 
 module.exports = router;
